ba769118e6c0f12c86aa25219b49241b0da7217e
Bonita Scott <bonitalscott@yahoo.com>
2021-02-06 17:16:00 -0500

    initial push

--- src/components/Jobs/Jobs.js
+++ src/components/Jobs/Jobs.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
 
 const Jobs = () => {
-    return (
-        <>
-        </>
-      );
-}
- 
+  return (
+    <>
+      <h1>Bonnie will work on this component</h1>
+    </>
+  );
+};
+
 export default Jobs;
