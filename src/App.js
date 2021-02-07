e922411cbb97496fa0b6d0a8987f13ad14be09e2
jesnadolski <nadolski.jessica@gmail.com>
2021-02-07 12:35:54 -0500

    working fetch and display

--- src/App.js
+++ src/App.js
@@ -7,29 +7,45 @@ import Zomato from "./components/Zomato/Zomato";
 
 function App() {
 
-const [location, setLocation] = useState('');
-const [latitude, setLatitude] = useState('');
-const [longitude, setLongitude] = useState('');
+// const [location, setLocation] = useState(undefined);
+const [coordinates, setCoordinates] = useState(undefined);
+const [latitude, setLatitude] = useState(undefined);
+const [longitude, setLongitude] = useState(undefined);
+const [loading, setLoading] = useState(true);
 
-const getLocation = () => {
+// const getLocation = () => {
+  // navigator.geolocation.getCurrentPosition(function(position) {
+  //   console.log("Latitude is:", position.coords.latitude);
+  //   console.log("Longitude is:", position.coords.longitude);
+  //   console.log(position);
+  //   setLatitude(position.coords.latitude);
+  //   setLongitude(position.coords.longitude);
+//   });
+// }
+
+useEffect(() => {
   navigator.geolocation.getCurrentPosition(function(position) {
     console.log("Latitude is:", position.coords.latitude);
     console.log("Longitude is:", position.coords.longitude);
     console.log(position);
     setLatitude(position.coords.latitude);
     setLongitude(position.coords.longitude);
-  });
-}
-
-useEffect(() => {
-  getLocation();
+    setCoordinates(position.coords);
+    setLoading(false);
+});
 }, []);
+
+if(loading && !coordinates) {
+  return (
+  <div>loading data...</div>
+  )
+}
 
   return (
           <div>
             <Nasa />
             <Weather />
-            <Zomato lat={setLatitude} lon={setLongitude}/>
+            <Zomato latlatitude={coordinates.latitude} longitude={coordinates.longitude}/>
             <Jobs />
     </div>
   );
--- src/components/Zomato/Zomato.js
+++ src/components/Zomato/Zomato.js
@@ -1,44 +1,44 @@
 import React, { useState, useEffect } from 'react';
+import {
+  CardColumns} from 'reactstrap';
+import ResResults from "./Restaurants";
 
 const Zomato = (props) => {
 
-  const [restaurants, setRestaurants] = useState([]);
+  const [restaurants, setRestaurants] = useState();
   
   function fetchZomato(){
     const key = "b3f53774cbedf32841880ac2c7fd6a6f";
     const baseurl = "https://developers.zomato.com/api/v2.1/geocode?"
-    const url=`${baseurl}'lat='${props.lat}'&lon='${props.lon}`;
-
-  
-    fetch('https://developers.zomato.com/api/v2.1/geocode?lat=39.96712960000001&lon=-86.10447359999999', {
+    const url=`${baseurl}'lat='${props.latitude}'&lon='${props.longitude}`;
+    console.log(url);
+      fetch('https://developers.zomato.com/api/v2.1/geocode?lat=39.96712960000001&lon=-86.10447359999999', {
       method: 'GET',
       headers: {
         'Accept': 'application/json',
-        'user-key': "b3f53774cbedf32841880ac2c7fd6a6f",
-        'Content-Type': 'application/json'
+        'user-key': "b3f53774cbedf32841880ac2c7fd6a6f"
       }
     })
     .then(response => response.json())
-    .then((json) => {
-      setRestaurants(json.results);
-      console.log(json.results);
+    .then((data) => {
+      setRestaurants(data);
+      console.log(data);
     })
   }
-
 
   useEffect(() => {
     fetchZomato()
   }, []);
 
 
-function displayRestaurants(){
-  return restaurants.length >0 ? restaurants.map((restaurant) => <Zomato zomatorestaurant={restaurant} />) : null;
-}
-
+// function displayRestaurants(){
+//   return restaurants.location.length >0 ? restaurants.map((restaurant) => <ResResults zomatorestaurant={restaurant} />) : null;
+// }
 
     return (
-        <>
-        </>
+      <div>
+   {restaurants.location.title}
+      </div>
       );
 }
  
