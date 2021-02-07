9649a366d0bf8a4a13ce5be4b67e8a4e666bfd2d
William Osman <wosman007@gmail.com>
2021-02-07 12:36:27 -0500

    adding will's fetch

--- src/App.js
+++ src/App.js
@@ -7,24 +7,28 @@ import Zomato from "./components/Zomato/Zomato";
 
 function App() {
 
-const [location, setLocation] = useState('');
+// const [location, setLocation] = useState(undefined);
+const [coordinates, setCoordinates] = useState(undefined);
+const [loading, setLoading] = useState(true);
 
-const getLocation = () => {
+
+useEffect(() => {
   navigator.geolocation.getCurrentPosition(function(position) {
     console.log("Latitude is:", position.coords.latitude);
     console.log("Longitude is:", position.coords.longitude);
     console.log(position);
+    setCoordinates(position.coords);
+    setLoading(false);
   });
-}
-
-useEffect(() => {
-  getLocation();
 }, []);
+if(loading && !coordinates) {
+  return <div>loading</div>
+}
 
   return (
           <div>
             <Nasa />
-            <Weather />
+            <Weather latitude={coordinates.latitude} longitude={coordinates.longitude} />
             <Zomato />
             <Jobs />
     </div>
--- src/components/Weather/Weather.js
+++ src/components/Weather/Weather.js
@@ -1,9 +1,29 @@
 import React, { useState, useEffect } from 'react';
 
-const Weather = () => {
+
+const Weather = (props) => {
+    const [weather, setWeather] = useState()
+
+    useEffect(() => {
+    function weatherFetch(){
+        const baseURL="https://api.openweathermap.org/data/2.5/"
+        const key = "fe544efe7738b3fecc6fdd574e208fe6"
+        const url= `${baseURL}weather?lat=${props.latitude}&lon=${props.longitude}&appid=${key}`
+        console.log(url)
+        fetch(url).then(res => res.json())
+        .then(json => {
+            setWeather(json)
+            console.log(json)
+            
+        })
+    }
+        weatherFetch();
+    }, [props.latitude, props.longitude])
+
     return ( 
         <>
         <h1>Will will work on this</h1>
+        {/* <div>{weather}</div> */}
         </>
      );
 }
