c2dd881617914709ff53b4ef2189205a8188cf58
Jonathan Cernero <cernero.jon@gmail.com>
2021-02-07 22:43:19 -0500

    update

--- src/components/Nasa/Nasa.js
+++ src/components/Nasa/Nasa.js
@@ -1,10 +1,33 @@
 import React, { useState, useEffect } from 'react'
+import { Container, Row, Col } from 'reactstrap'
 
-const Nasa = () => {
+const Nasa = props => {
+  const [data, setData] = useState()
+
+  useEffect(() => {
+    function nasaFetch() {
+      const baseURL = 'https://api.nasa.gov/planetary/earth/'
+      const key = 'mgd2pnpv3V2ChJuZUdrjTKMtADdVsWH9oHzRCVhq'
+      const url = `${baseURL}imagery?lon=${props.longitude}&lat=${props.latitude}&date=2020-12-01&api_key=${key}`
+      console.log(url)
+      fetch(url)
+        .then(res => res.blob())
+        .then(result => {
+          setData(URL.createObjectURL(result))
+        })
+        .catch(err => console.log(err))
+    }
+    nasaFetch()
+  }, [props.latitude, props.longitude])
+
   return (
-    <>
-      <h1>Jonathan's Working On This Fetch</h1>
-    </>
+    <Container>
+      <Row>
+        <Col md='4' className='mb-3'>
+          <img src={data} alt='' />
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
