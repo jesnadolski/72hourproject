d0179d60c8cf3cb024449ef9fc7100d46a6b91af
Jonathan Cernero <cernero.jon@gmail.com>
2021-02-08 22:37:25 -0500

    Adding Map to Jobs

--- src/components/Jobs/Jobs.js
+++ src/components/Jobs/Jobs.js
@@ -1,10 +1,44 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
 
-const Jobs = () => {
-    return (
-        <>
-        </>
-      );
+const Jobs = props => {
+  const [results, setResults] = useState([])
+
+  const corsURL = 'https://efa-cors-anywhere.herokuapp.com/'
+  const baseURL = 'https://jobs.github.com/positions'
+
+  useEffect(() => {
+    const url = `${baseURL}?lat=${props.latitude}&long=${props.longitude}`
+
+    function fetchJobs() {
+      if (props.latitude && props.longitude) {
+        fetch(corsURL + url, {
+          method: 'GET',
+          headers: {
+            Accept: 'application/json'
+          }
+        })
+          .then(res => res.json())
+          .then(data => {
+            setResults(data)
+            console.log(data)
+          })
+      }
+    }
+    fetchJobs()
+  }, [props])
+
+  return (
+    <div>
+      {results.map((job, index) => {
+        return (
+          <div key={index}>
+            <h3>{job.company}</h3>
+            <p>{[job.location]}</p>
+          </div>
+        )
+      })}
+    </div>
+  )
 }
- 
-export default Jobs;
+
+export default Jobs
