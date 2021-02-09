f704df569bfefff829998142408954d4118f6d6e
Bonita Scott <bonitalscott@yahoo.com>
2021-02-08 22:31:48 -0500

    Update Jobs.js

--- src/App.js
+++ src/App.js
@@ -1,36 +1,33 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-import Jobs from "./components/Jobs/Jobs";
-import Nasa from "./components/Nasa/Nasa";
-import Weather from "./components/Weather/Weather";
-import Zomato from "./components/Zomato/Zomato";
+import Jobs from './components/Jobs/Jobs';
+import Nasa from './components/Nasa/Nasa';
+import Weather from './components/Weather/Weather';
+import Zomato from './components/Zomato/Zomato';
 
 function App() {
+  const [coordinates, setCoordinates] = useState(undefined);
+  const [loading, setLoading] = useState(true);
 
-
-const [coordinates, setCoordinates] = useState(undefined);
-const [loading, setLoading] = useState(true);
-
-useEffect(() => {
-  navigator.geolocation.getCurrentPosition(function(position) {
-    console.log("Latitude is:", position.coords.latitude);
-    console.log("Longitude is:", position.coords.longitude);
-    console.log(position);
-    setCoordinates(position.coords);
-    setLoading(false);
-  });
-}, []);
-if(loading && !coordinates) {
-  return <div>loading</div>
-}
-
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      console.log('Latitude is:', position.coords.latitude);
+      console.log('Longitude is:', position.coords.longitude);
+      console.log(position);
+      setCoordinates(position.coords);
+      setLoading(false);
+    });
+  }, []);
+  if (loading && !coordinates) {
+    return <div>loading</div>;
+  }
 
   return (
-          <div>
-            <Nasa latitude={coordinates.latitude} longitude={coordinates.longitude}/>
+    <div>
+      {/* <Nasa latitude={coordinates.latitude} longitude={coordinates.longitude}/>
             <Weather latitude={coordinates.latitude} longitude={coordinates.longitude} />
-            <Zomato latlatitude={coordinates.latitude} longitude={coordinates.longitude}/>
-            <Jobs latitude={coordinates.latitude} longitude={coordinates.longitude}/>
+            <Zomato latlatitude={coordinates.latitude} longitude={coordinates.longitude}/> */}
+      <Jobs latitude={coordinates.latitude} longitude={coordinates.longitude} />
     </div>
   );
 }
--- src/components/Jobs/Jobs.js
+++ src/components/Jobs/Jobs.js
@@ -1,11 +1,34 @@
 import React, { useState, useEffect } from 'react';
 
-const Jobs = () => {
-  return (
-    <>
-      <h1>Bonnie will work on this component</h1>
-    </>
-  );
+const Jobs = (props) => {
+  // const [location, setLocation] = useState();
+  // const [title, setTitle] = useState();
+  // const [jobid, setJobId] = useState();
+
+  const [results, setResults] = useState([]);
+  const corsURL = 'https://efa-cors-anywhere.herokuapp.com/';
+  const baseURL = 'https://jobs.github.com/positions';
+  const url = `${baseURL}?lat=${props.latitude}&long=${props.longitude}`;
+
+  function fetchJobs() {
+    fetch(corsURL + url, {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setResults(data);
+        console.log(data);
+      });
+  }
+
+  useEffect(() => {
+    fetchJobs();
+  }, [props]);
+
+  return <></>;
 };
 
 export default Jobs;
