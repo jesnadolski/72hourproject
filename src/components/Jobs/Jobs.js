ee776b49d07bfc74438366bd9b9e5292acf24319
Bonita Scott <bonitalscott@yahoo.com>
2021-02-09 19:21:29 -0500

    adding mapping in return

--- src/components/Jobs/Jobs.js
+++ src/components/Jobs/Jobs.js
@@ -1,32 +1,31 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 
+const Jobs = (props) => {
+  const [results, setResults] = useState([]);
 
-const Jobs = props => {
-  const [results, setResults] = useState([])
-
-  const corsURL = 'https://efa-cors-anywhere.herokuapp.com/'
-  const baseURL = 'https://jobs.github.com/positions'
+  const corsURL = 'https://efa-cors-anywhere.herokuapp.com/';
+  const baseURL = 'https://jobs.github.com/positions';
 
   useEffect(() => {
-    const url = `${baseURL}?lat=${props.latitude}&long=${props.longitude}`
+    const url = `${baseURL}?lat=${props.latitude}&long=${props.longitude}`;
 
     function fetchJobs() {
       if (props.latitude && props.longitude) {
         fetch(corsURL + url, {
           method: 'GET',
           headers: {
-            Accept: 'application/json'
-          }
+            Accept: 'application/json',
+          },
         })
-          .then(res => res.json())
-          .then(data => {
-            setResults(data)
-            console.log(data)
-          })
+          .then((res) => res.json())
+          .then((data) => {
+            setResults(data);
+            console.log(data);
+          });
       }
     }
-    fetchJobs()
-  }, [props])
+    fetchJobs();
+  }, [props]);
 
   return (
     <div>
@@ -34,12 +33,16 @@ const Jobs = props => {
         return (
           <div key={index}>
             <h3>{job.company}</h3>
-            <p>{[job.location]}</p>
+            <p>{job.location}</p>
+            <p>{[job.title]}</p>
+            <p>
+              <a href={job.url}>Click to see full job description.</a>
+            </p>
           </div>
-        )
+        );
       })}
     </div>
-  )
-}
+  );
+};
 
 export default Jobs;
